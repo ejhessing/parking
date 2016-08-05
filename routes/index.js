bf45ecec9d5f37cd78eaf6e14481b539db22b685
EDA <eda@mac-mini-2.local>
2016-08-05 12:09:17 +1200

    Qr code for test added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var db = require('../db/db')
+var qr = require('qr-image')
 
 module.exports = {
   get: get,
@@ -14,10 +15,10 @@ function searchCar (req, res) {
   var rego = req.query.rego
   db.getUserInfo(rego)
   .then( function (data) {
+    data[0].qr = qr.svgObject("http://www.trademe.co.nz")
     res.render('profile', data[0])
   })
   .catch(function (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   })
-
 }
