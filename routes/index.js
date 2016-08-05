5fa08720227ffc055b64d31d2bd8b46ce2eeaea0
EDA <eda@mac-mini-2.local>
2016-08-05 12:12:43 +1200

    Qr Code done

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ function searchCar (req, res) {
   var rego = req.query.rego
   db.getUserInfo(rego)
   .then( function (data) {
-    data[0].qr = qr.svgObject("http://www.trademe.co.nz")
+    data[0].qr = qr.svgObject("sms/" + data[0].user_id)
     res.render('profile', data[0])
   })
   .catch(function (err) {
